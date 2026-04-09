c9d5db9aa57474df97b2bbefa42f20c914845e0c
Vini <vinicius.g.dev@outlook.com>
2026-04-08 23:11:47 -0400

    add Ra e Rb

--- resistência_de_materiais.py
+++ resistência_de_materiais.py
@@ -1,29 +1,63 @@
 class viga:
-    def __init__ (self, comprimento):
+    def __init__ (self, comprimento, tipo_apoio="biapoiada"):
         self.Comprimento = comprimento
+        self.tipo_apoio = tipo_apoio
         self.apoios = []
         self.cargas = []
-        self.reacoes = {"Ra": 0, "Rb": 0}
+        self.reacoes = {"Ra": 0, "Rb": 0, "Ma": 0}
+
+    def add_carga(self, carga):
+        self.cargas.append(carga)
+
+    def calcular_reacoes(self, posA, posB=0):
+        somaFy = 0
+        somaM = 0
+
+        for c in self.cargas:
+            if isinstance(c, CarregamentoConcentrado):
+                F = c.Forca
+                X = c.Posicao
+            elif isinstance(c, CarregamentoDistribuido):
+                largura = c.PosicaoF - c.PosicaoI
+                F = (c.ForcaI + c.ForcaF) * largura / 2
+                centroide_local = (largura * (c.ForcaI + 2 * c.ForcaF) / (3 * (c.ForcaI + c.ForcaF)))
+                X = c.PosicaoI + centroide_local
+            
+            somaFy += F
+            somaM += F * (X - posA)
+
+        if self.tipo_apoio == "biapoiada":
+            dist_apoios = posB - posA
+            self.reacoes["Rb"] = somaM / dist_apoios
+            self.reacoes["Ra"] = somaFy - self.reacoes["Rb"]
+            self.reacoes["Ma"] = 0
+            
+        elif self.tipo_apoio == "engastada":
+            self.reacoes["Ra"] = somaFy
+            self.reacoes["Ma"] = somaM
+            self.reacoes["Rb"] = 0
 
 class CarregamentoConcentrado:
-    def __init__ (self, forca, posição):
+    def __init__ (self, forca, posicao):
         self.Forca = forca
-        self.Posicao = posição
-    
-    #retorna a saida
-    def info(self):
-        return f"{self.Forca} {self.Posicao}"
+        self.Posicao = posicao
         
 class CarregamentoDistribuido:
-    def __init__ (self, forçaI, forcaF, posicaoI, posicaoF):
+    def __init__ (self, forcaI, forcaF, posicaoI, posicaoF):
         self.ForcaI = forcaI
-        self.FrocaF = forcaF
+        self.ForcaF = forcaF
         self.PosicaoI = posicaoI
         self.PosicaoF = posicaoF
 
-#teste de saida
-valor1 = input('digite o valor da força')
-valor2 = input('digite o valor da posição')
+minha_viga = viga(6.0, tipo_apoio="biapoiada")
 
-resultado = CarregamentoConcentrado(valor1, valor2)
-print(resultado.info())       
+carga1 = CarregamentoDistribuido(0.0, 3.0, 0.0, 2.0)
+minha_viga.add_carga(carga1)
+
+carga2 = CarregamentoDistribuido(3.0, 3.0, 4.0, 6.0)
+minha_viga.add_carga(carga2)
+
+minha_viga.calcular_reacoes(2.0, 6.0)
+
+print(f"Ra: {minha_viga.reacoes['Ra']:.2f} kN")
+print(f"Rb: {minha_viga.reacoes['Rb']:.2f} kN")    
